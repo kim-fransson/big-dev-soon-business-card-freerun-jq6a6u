314fcd48163af41efe80088b3ec0d12ab76d27d9
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-11-13 16:55:31 +0100

    Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { ContactSection } from "./components/sections";
+
 export default function App() {
   return (
     <div className="bg-green-200 min-h-screen flex items-center justify-center">
-      <main className="bg-white rounded-3xl max-w-3xl w-full shadow-2xl h-96"></main>
+      <main className="bg-white rounded-3xl max-w-5xl w-full shadow-2xl overflow-hidden">
+        <ContactSection />
+      </main>
     </div>
   );
 }
---
+++ src/components/sections/ContactSection.jsx
@@ -0,0 +1,47 @@
+import EmailIcon from "../../assets/icons/email-icon.svg?react";
+import GithubIcon from "../../assets/icons/github-icon.svg?react";
+import LinkedinIcon from "../../assets/icons/linkedin-icon.svg?react";
+
+const ContactLink = ({ href, children }) => {
+  return (
+    <a
+      className="text-gray-300 transition-colors hover:text-white outline-none border focus-visible:border-white border-transparent focus-visible:text-white rounded-md"
+      target="_blank"
+      rel="noreferrer"
+      href={href}
+    >
+      {children}
+    </a>
+  );
+};
+
+export const ContactSection = () => {
+  return (
+    <section className="bg-green-700 max-w-xs text-white text-sm flex flex-col gap-4 px-10 pt-96 pb-16">
+      <div className="flex flex-col gap-1">
+        <div className="flex gap-1">
+          <EmailIcon />
+          Email
+        </div>
+        <ContactLink href="mailto:kim.fransson.22@gmail.com">
+          kim.fransson.22@gmail.com
+        </ContactLink>
+      </div>
+
+      <hr className="border-8 border-gray-100 -mx-10" />
+
+      <div className="flex flex-col gap-1">
+        Find me on
+        <div className="flex gap-3">
+          <ContactLink href="https://github.com/kim-fransson">
+            <GithubIcon />
+          </ContactLink>
+
+          <ContactLink href="https://www.linkedin.com/in/kim-fransson-b714ba215/">
+            <LinkedinIcon />
+          </ContactLink>
+        </div>
+      </div>
+    </section>
+  );
+};
